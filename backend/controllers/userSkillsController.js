d3a3559810a4993dace0fa7fe77ea1a3e5e56212
Senal Perera <senalperera777@gmail.com>
2026-02-19 17:33:08 +0530

    Added 2 more controllers + fixed userSkillsController bugs
    - Added avatarController
    - Added skillController
    - Fixed some semantic errors + added missing necessary code for userSkillsController

--- backend/controllers/userSkillsController.js
+++ backend/controllers/userSkillsController.js
@@ -1,15 +1,19 @@
-const SkillData = require("../models/UserSkills.js");
+const UserSkill = require("../models/UserSkills.js");
+const Skill = require("../models/Skill.js");
+const User = require("../models/UserAccountDetails.js");
 
-exports.createSkill = async (req, res) => {
+exports.getAllUserSkills = async (req, res) => {
   try {
-    // Creating skill
-    const newSkill = await SkillData.create(req.body);
-    res.status(201).json({
+    const userSkills = await UserSkill.find({
+      userId: req.params.userId,
+    }).populate("skillId", "name skillPoint classId");
+    res.status(200).json({
       status: "success",
-      data: { skill: newSkill },
+      results: userSkills.length,
+      data: userSkills,
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(500).json({
       status: "fail",
       message: err.message,
     });
@@ -19,7 +23,7 @@ exports.createSkill = async (req, res) => {
 exports.getUserSkillData = async (req, res) => {
   try {
     // Reading the skill
-    const skill = await SkillData.findById(req.params.id);
+    const skill = await UserSkill.findById(req.params.id);
     if (!skill) {
       return res.status(404).json({
         status: "fail",
@@ -29,7 +33,7 @@ exports.getUserSkillData = async (req, res) => {
 
     res.status(200).json({
       status: "success",
-      data: { destination },
+      data: { skill },
     });
   } catch (err) {
     res.status(400).json({
@@ -39,22 +43,26 @@ exports.getUserSkillData = async (req, res) => {
   }
 };
 
-exports.updateUserSkillData = async (req, res) => {
+exports.updateUserSkill = async (req, res) => {
   try {
     // Updating destination
-    const updatedSkills = await SkillData.findByIdAndUpdate(
+    const updatedSkill = await UserSkill.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true, runValidators: true },
     );
 
     // Checking if destination exists
-    if (!updatedSkills) {
+    if (!updatedSkill) {
       return res.status(404).json({
         status: "fail",
         message: "Skill not found",
       });
     }
+    res.status(200).json({
+      status: "success",
+      data: updatedSkill,
+    });
   } catch (err) {
     res.status(400).json({
       status: "fail",
@@ -63,21 +71,21 @@ exports.updateUserSkillData = async (req, res) => {
   }
 };
 
-exports.deleteUserSkillData = async (req, res) => {
+exports.removeUserSkill = async (req, res) => {
   try {
-    const skillToDelete = await SkillData.findById(req.params.id);
+    const skill = await UserSkill.findById(req.params.id);
     // Checking if document exists before deleting
-    if (!skillToDelete) {
+    if (!skill) {
       return res.status(404).json({
         status: "fail",
         message: "Skill not found",
       });
     }
     // Logging the data deleted
-    console.log(skillToDelete);
+    console.log(skill);
 
     // Authentication check
-    if (skillToDelete.createdBy.toString() !== req.user.id) {
+    if (skill.userId.toString() !== req.user.id) {
       return res.status(403).json({
         status: "fail",
         message: "You do not have permission to delete this skill",
@@ -85,7 +93,7 @@ exports.deleteUserSkillData = async (req, res) => {
     }
 
     // Deletion
-    await skillToDelete.deleteOne();
+    await skill.deleteOne();
     res.status(204).json({
       status: "success",
       data: null,
@@ -98,6 +106,88 @@ exports.deleteUserSkillData = async (req, res) => {
   }
 };
 
-exports.getAllUserSkillData = async (req, res) => {
-  const skills = await SkillData.find({ active: { $ne: false } });
+// Update skill level
+exports.updateUserSkillLevel = async (req, res) => {
+  try {
+    const userSkillLevel = await UserSkill.findByIdAndUpdate(
+      req.params.id,
+      { level: req.body.level },
+      { new: true, runValidators: true },
+    );
+    if (!userSkillLevel) {
+      return res.status(404).json({
+        status: "fail",
+        message: "UserSkill not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: userSkillLevel,
+    });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err.message });
+  }
+};
+
+exports.unlockSkill = async (req, res) => {
+  try {
+    const { userId, skillId } = req.body;
+
+    // Finding skill and checking if it exists
+    const skill = await Skill.findById(skillId);
+    if (!skill) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Skill not found",
+      });
+    }
+    if (skill.status == "locked") {
+      return res.status(400).json({
+        status: "fail",
+        message: "This skill is not yet unlocked",
+      });
+    }
+
+    // Checking if user has enough skill points
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+
+    if (user.skillPoints < skill.skillPoints) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Not enough skill points",
+      });
+    }
+
+    // Subtracting skills points from user if successful
+    user.skillPoints -= skill.skillPoint;
+    await user.save();
+
+    // Making the userSkill record
+    const userSkill = await UserSkill.create({
+      userId,
+      skillId,
+      name: skill.name,
+    });
+    res.status(201).json({
+      status: "success",
+      data: {
+        userSkill,
+        remainingSkillPoints: user.skillPoints,
+      },
+    });
+  } catch (err) {
+    if (err.code === 11000) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Skill already unlocked",
+      });
+    }
+    res.status(400).json({ status: "fail", message: err.message });
+  }
 };
