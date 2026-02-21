7625f6ccf1d00041548f09e93aa1dc1b194dd5e5
Yasara Sachinthaka Perera <yasaraboii@gmail.com>
2026-02-21 12:00:52 +0530

    Controllers and Routes
    Finished the rest of the controllers and Routes and added to the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const connectToDatabase = require('./db');
+const avatarRoutes = require('./routes/avatarRoutes');
+const destinationRoutes = require('./routes/destinationRoutes');
+const questRoutes = require('./routes/questRoutes');
+const skillRoutes = require('./routes/skillRoutes');
 const userAccountDetailsRoutes = require('./routes/userAccountDetailsRoutes');
+const userGroupRoutes = require('./routes/userGroupRoutes');
+const userskillRoutes = require('./routes/userSkillRoutes');
+const userTripRoutes = require('./routes/userTripRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/user-account-details', userAccountDetailsRoutes);
+app.use('/api/avatars', avatarRoutes);
+app.use('/api/destinations', destinationRoutes);
+app.use('/api/quests', questRoutes);
+app.use('/api/skills', skillRoutes);
+app.use('/api/user-groups', userGroupRoutes);
+app.use('/api/user-skills', userskillRoutes);
+app.use('/api/user-trips', userTripRoutes);
 
 async function startApp() {
   await connectToDatabase();
