82eb2e33f2ed82885e535eaa50ff948d8d58bf1a
Senal Perera <senalperera777@gmail.com>
2026-03-12 18:09:34 +0530

    feat: added moving gradient background widget
    - This can be implemented into any page without having to manually code it

---
+++ lib/widgets/animated_gradient_background.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class AnimatedGradientBackground extends StatefulWidget {
+  final Widget child;
+  const AnimatedGradientBackground({super.key, required this.child});
+
+  @override
+  State<AnimatedGradientBackground> createState() =>
+      _AnimatedGradientBackgroundState();
+}
+
+class _AnimatedGradientBackgroundState extends State<AnimatedGradientBackground>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Alignment> _topAlignment;
+  late Animation<Alignment> _bottomAlignment;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    )..repeat(reverse: true);
+
+    _topAlignment = TweenSequence<Alignment>([
+      TweenSequenceItem(
+        tween: Tween<Alignment>(
+          begin: Alignment.topLeft,
+          end: Alignment.topRight,
+        ),
+        weight: 1,
+      ),
+    ]).animate(_controller);
+
+    _bottomAlignment = TweenSequence<Alignment>([
+      TweenSequenceItem(
+        tween: Tween<Alignment>(
+          begin: Alignment.bottomRight,
+          end: Alignment.bottomLeft,
+        ),
+        weight: 1,
+      ),
+    ]).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: _topAlignment.value,
+              end: _bottomAlignment.value,
+              colors: const [
+                Color(0xFF0A0A0F),
+                Color(0xFF1A0A2E),
+                Color(0xFF551161),
+              ],
+            ),
+          ),
+          child: child,
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
