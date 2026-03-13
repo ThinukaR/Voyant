e3461893fe189c770b190883a6355963608b7d54
Thinuka Rambukwella <thinukaananda@gmail.com>
2026-03-13 19:08:48 +0530

    error fixing and database logging
    logging info in the database ( initial entry ) and adjusting indentations

--- functions/index.js
+++ functions/index.js
@@ -26,40 +26,60 @@ function formatDate(timestamp) {
   const dateConvert = timestamp.toDate(); //converted to a date object 
   const y = dateConvert.getUTCFullYear();
   //pad start makes sure it is 2 digits instead of sometimes 1
-  const m = String(d.getUTCMonth() + 1).padStart(2, "0");
+  const m = String(dateConvert.getUTCMonth() + 1).padStart(2, "0");
   const d = String(dateConvert.getUTCDate()).padStart(2,"0");
-  return '${y}-${m}-${day}';
+  return `${y}-${m}-${d}`;
 }
 
 //Auth checks 
 exports.initBusinessPartnerDashboard = functions.https.onCall(async (data, context) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError("unauthenticated", "user must be signed in."); //to execute if user is not authenticated
-  }
-});
-
-const uid = context.auth.uid; //retrieve user ID 
-const userRef = db.collection("users").doc(uid); //creates a reference to the user(based on UID) document ( basically a pointer)
-//creates a ref to the business partner data of the app
-const partnerRef = db.collection("business_partner_info").doc(uid); 
-
-const [userSnap, partnerSnap] = await Promise.all([userRef.get(), partnerRef.get()]);
-
-const isBusinessPartner = userSnap.exists && userSnap.data().isBusinessPartner === true;
-  if (!isBusinessPartner) {
-    throw new functions.https.HttpsError("permission-denied", "is not a business partner.");
+    if (!context.auth) {
+      throw new functions.https.HttpsError("unauthenticated", "user must be signed in."); //to execute if user is not authenticated
   }
 
-  if (partnerSnap.exists) {
-    return { 
-      success: true, 
-      alreadyExists: true };
-  }
-
-//calculation for expiry 
   const now = admin.firestore.Timestamp.now();
-  const expiresAt = admin.firestore.Timestamp.fromMillis(
-    now.toMillis() + CODE_EXPIRY_TIME * 60 * 1000
-  );
+
+  const uid = context.auth.uid; //retrieve user ID 
+  const userRef = db.collection("users").doc(uid); //creates a reference to the user(based on UID) document ( basically a pointer)
+  //creates a ref to the business partner data of the app
+  const partnerRef = db.collection("business_partner_info").doc(uid); 
+
+  const [userSnap, partnerSnap] = await Promise.all([userRef.get(), partnerRef.get()]);
+
+  const isBusinessPartner = userSnap.exists && userSnap.data().isBusinessPartner === true;
+    if (!isBusinessPartner) {
+      throw new functions.https.HttpsError("permission-denied", "is not a business partner.");
+    }
+
+    if (partnerSnap.exists) {
+      return { 
+        success: true, 
+        alreadyExists: true };
+    }
+
+  //calculation for expiry 
+    const expiresAt = admin.firestore.Timestamp.fromMillis(
+      now.toMillis() + CODE_EXPIRY_TIME * 60 * 1000
+    );
+
+    await partnerRef.set({
+      currentCode:  createBusinessCode(),
+      codeExpiresAt: expiresAt,
+      redemptions: 0,
+      maxRedemptionsPerCode: MAX_REDEMPTIONS,
+      refreshCount: 1,
+      lastRefresh: now,
+      refreshLimitPerHour: MAX_REFRESHES_HOURLY,
+      dailyRedemptions: 0,
+      totalRedemptions: 0,
+      lastResetDate: formatDate(now),
+      recentRedemptionTimestamps: [],
+    });
+
+    return {success: true , alreadyExists: true};
+  }
+);
+
+
 
  
