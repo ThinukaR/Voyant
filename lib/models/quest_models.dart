649089000ba434c1b627498bf4b8fb519e100905
Thinuka Rambukwella <thinukaananda@gmail.com>
2026-03-19 16:47:11 +0530

    feat: update quest model folder ( subquest , dialoguenode, dialogoptions)
    Updated the quest models folder with the sub quest model, dialogue nodes and options

--- lib/models/quest_models.dart
+++ lib/models/quest_models.dart
@@ -24,3 +24,61 @@ class MainQuest with _$MainQuest {
 
   factory MainQuest.fromJson(Map<String, dynamic> json) => _$MainQuestFromJson(json);
 }
+
+@freezed
+class SubQuest with _$SubQuest {
+  const factory SubQuest({
+    required String id,
+    required String mainQuestId,
+    required String title,
+    required String description,
+    required int questOrder,
+    required QuestLocation location,
+    required NPC npc,
+    required String type,
+    required List<DialogueNode> dialogueNodes,
+    required String startDialogueId,
+    required bool isCompleted,
+    required CompletionConditions completionConditions,
+    required QuestRewards rewards,
+    required QuestPrerequisites prerequisites,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) = _SubQuest;
+
+  factory SubQuest.fromJson(Map<String, dynamic> json) => _$SubQuestFromJson(json);
+}
+
+@freezed  
+class DialogueNode with _$DialogueNode {
+
+  const factory DialogueNode({
+    required String id,
+    required String npcName,
+    required String npcAvatar,
+    required String dialogueText,
+    required String emotion,
+    required List<DialogueOption> options,
+    required bool isAutoAdvance,
+    required int autoAdvanceDelay,
+  }) = _DialogueNode;
+
+  factory DialogueNode.fromJson(Map<String, dynamic> json) => _$DialogueNodeFromJson(json);
+}
+
+@freezed
+class DialogueOption with _$DialogueOption {
+
+  const factory DialogueOption({
+    required String id,
+    required String text,
+    required String type,
+    String? nextDialogueId,
+    required DialogueAction action,
+    required DialogueConditions conditions,
+    required DialogueConsequences consequences,
+
+  }) = _DialogueOption;
+
+  factory DialogueOption.fromJson(Map<String, dynamic> json) => _$DialogueOptionFromJson(json);
+}
